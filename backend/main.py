5179870f3cbbca5867e58f61a5b1c25ac1d2722a
Ruan Pretorius <ruan@melio.ai>
2024-01-25 16:22:03 +0200

    Add docstrings and accept base64 encoded pdf

--- backend/main.py
+++ backend/main.py
@@ -1,22 +1,29 @@
+"""
+RAG-QA backend logic.
+"""
+
 from fastapi import FastAPI
 from pydantic import BaseModel
-from src.utils import url_qa
+from src.utils import get_answer
 
 app = FastAPI()
 
+
 class Question(BaseModel):
+    """Question object used for RAG QA."""
+
     question: str
-    url: str
-    
+    pdf: str
+
 
 @app.get("/")
 def root():
-    return {"Status": "Server running."}
+    """Check status of backend server."""
+    return {"status": "Server running."}
+
 
 @app.post("/ask")
-def ask_question(question:Question):
-    ans = url_qa(
-        url=question.url,
-        question=question.question
-    )
+def ask_question(question: Question):
+    """Run the QA RAG cycle on a document and return the answer."""
+    ans = get_answer(pdf_string=question.pdf, question=question.question)
     return ans
